39492b2da962fe77a5931689a45146c63764bf4f
arjunsingh14 <arjunsingh140601@gmail.com>
2022-05-17 19:12:16 +0800

    relX and relY are decimals

--- src/data.js
+++ src/data.js
@@ -7,9 +7,9 @@ import link from "./assets/link.png"
 import uniqid from "uniqid";
 
 export const characters = [
-  { id: uniqid(), name: "Spider-man", url: wallcrawler, found: false },
-  { id: uniqid(), name: "Brian", url: brian, found: false },
-  { id: uniqid(), name: "Link", url: link,  found: false },
-  { id: uniqid(), name: "Tom", url: tom ,found: false },
-  { id: uniqid(), name:"Neo" , url: neo ,found:  false},
+  { id: uniqid(), name: "Spider-man", url: wallcrawler, found: false, relX: 0.6464, relY: 0.5563 },
+  { id: uniqid(), name: "Brian", url: brian, found: false, relX: 0.14, relY: 0.723},
+  { id: uniqid(), name: "Link", url: link, found: false, relX: 0.23, relY: 0.6134 },
+  { id: uniqid(), name: "Tom", url: tom, found: false, relX: 0.86, relY: 0.656 },
+  { id: uniqid(), name: "Neo", url: neo, found: false, relX: 0.41, relY: 0.22 },
 ];
